6eac00dd3797820d2da86f24741416cd51a8e891
Todd H <github@simplybest.com>
2022-09-16 15:47:31 -0600

    switch components to setup script

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,6 @@
   </ion-app>
 </template>
 
-<script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    IonApp,
-    IonRouterOutlet
-  }
-});
+<script lang="ts" setup>
+  import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -17,13 +17,7 @@
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+<script lang="ts" setup>
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
